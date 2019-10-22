69c5a0dd884186af48d57416814ece25b43d5847
shigas <shigasuresh546@gmail.com>
2019-10-22 23:55:58 +0530

    adding extension for user defaults to avoid three issues

--- TypeSafe-UserDefaults/ViewController.swift
+++ TypeSafe-UserDefaults/ViewController.swift
@@ -13,8 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UserDefaults.standard.userID = "123"
+        if let userID = UserDefaults.standard.userID {
+            print(userID)
+        }
     }
-
-
 }
 
+// 1. avoid wrong key value
+// 2. avoid wrong type cast
+// 3. avoid keys over lapping
+
+extension UserDefaults {
+    var userID: String? {
+        get{
+            return string(forKey: #function)
+        }
+        set{
+            set(newValue, forKey: #function)
+        }
+    }
+    
+}
